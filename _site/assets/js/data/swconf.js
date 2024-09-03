c88ac83bf05da86e9ba093781cd0dc14b595d3da
huangguansong <guansong.huang@momenta.ai>
2024-09-03 21:05:03 +0800

    Revert "https://dillinger.io/示例"
    This reverts commit 4c23fc3681c8b598cb87505d2199e2cee49100f0.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725368360',resources: [
+    cacheName: 'chirpy-1724752693',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
